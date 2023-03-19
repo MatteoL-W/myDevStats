9157857e1682592d3929b6b8e7bf061c8cc8c36a
Matteo Leclercq <matteo.ftt@gmail.com>
2023-03-19 17:58:37 +0100

    ✨[RepoVisu] Fetch latest commits

--- src/components/ProfileDataVisualization.vue
+++ src/components/ProfileDataVisualization.vue
@@ -1,54 +1,60 @@
 <template>
   <div class="info__data" ref="threejs">
-    <renderer ref="renderer" antialias orbit-ctrl resize="true" alpha>
-      <camera :position="{ z: 10, y:3 }"></camera>
-      <scene>
-        <point-light :position="{ y: 50, z: 50 }"></point-light>
-        <box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
-          <LambertMaterial />
-        </box>
+    <Renderer ref="renderer" antialias orbit-ctrl resize="true" alpha>
+      <Camera :position="{ z: 11, y: 4 }"></Camera>
+      <Scene ref="scene">
+        <AmbientLight :intensity="0.4"></AmbientLight>
+        <PointLight :position="{ y: 10, z: 10 }" :intensity="0.4"></PointLight>
+
         <Text v-if="selectedRepo" :text="selectedRepo.name"
-            font-src="https://unpkg.com/three@0.77.0/examples/fonts/helvetiker_bold.typeface.json"
-            align="center"
-            :size="1"
-            :height="0.5"
-            :position="{ x: 0, y: 3, z: 0 }"
-            :cast-shadow="true"
+              font-src="https://unpkg.com/three@0.77.0/examples/fonts/helvetiker_bold.typeface.json"
+              align="center"
+              :size="1"
+              :height="0.2"
+              :position="{ x: 0, y: 3, z: 0 }"
+              :cast-shadow="true"
         >
-          <PhongMaterial />
+          <PhongMaterial color="#ff0000"/>
         </Text>
-      </scene>
-    </renderer>
+
+        <Sphere v-for="(commit, index) in latestCommits" :key="commit.name" :radius="0.3"
+                :position="{ x: index - (latestCommits.length/2) + 0.5, y: 0, z: 0 }">
+          <PhongMaterial color="#ff0000"/>
+        </Sphere>
+      </Scene>
+    </Renderer>
   </div>
 </template>
 
 <script>
-import { eventBus } from '@/utils/event-bus-profile.js'
+import {eventBus} from '@/utils/event-bus-profile.js'
+import {octokit} from "@/services/api/octokit";
+import * as THREE from "three";
 
 export default {
   name: "ProfileDataVisualization",
   data() {
     return {
       selectedRepo: null,
-      exist: true,
+      latestCommits: [],
     }
   },
   props: {
     username: {type: String, required: true},
   },
-  mounted() {
-    eventBus.on('repo-selected', repo => {
+  async mounted() {
+    const scene = this.$refs.scene
+
+    eventBus.on('repo-selected', async repo => {
       this.selectedRepo = repo
+
+      const latestCommits = await octokit.request('GET /repos/{owner}/{repo}/commits?author={username}', { owner: repo.owner.login, repo: repo.name, username: this.username })
+      this.latestCommits = latestCommits.data
+      //console.log(this.latestCommits)
+
+      const gridHelper = new THREE.GridHelper( Math.min(this.latestCommits.length, 30), Math.min(this.latestCommits.length, 30) );
+      scene.add( gridHelper );
     })
-
-    const renderer = this.$refs.renderer
-    const box = this.$refs.box.mesh
-
-    renderer.onBeforeRender(() => {
-      box.rotation.x += 0.01
-    })
-
-    //retrieveRepoInfo(this.username, "arkulib").then((result) => {console.log(result)})
   },
 }
 
--- src/utils/github.js
+++ src/utils/github.js
@@ -1,11 +1,18 @@
 import { octokit } from '@/services/api/octokit'
 
-const countCommits = async (
-  repoAuthor, repo, branch, username = repoAuthor) => {
-  const response = await octokit.request(
-    'GET /repos/{repoAuthor}/{repo}/commits?author={username}&sha={branch}&per_page=1&page=1',
-    { repoAuthor, repo, branch, username },
-  )
+const countCommits = async (repoAuthor, repo, branch, username) => {
+  let response;
+  if (username) {
+    response = await octokit.request(
+        'GET /repos/{repoAuthor}/{repo}/commits?author={username}&sha={branch}&per_page=1&page=1',
+        { repoAuthor, repo, branch, username },
+    )
+  } else {
+    response = await octokit.request(
+        'GET /repos/{repoAuthor}/{repo}/commits?sha={branch}&per_page=1&page=1',
+        { repoAuthor, repo, branch, username },
+    )
+  }
 
   const link = response.headers.link
   // We get the page number generated in the header response in order to have the commits number
@@ -17,6 +24,8 @@ const retrieveCommitsNumber = async (repositories, author) => {
   for (let repository of repositories) {
     repository.commitsNumber = await countCommits(repository.owner.login,
       repository.name, repository.default_branch, author)
+    repository.allCommitsNumber = await countCommits(repository.owner.login,
+        repository.name, repository.default_branch, null)
   }
 }
 
